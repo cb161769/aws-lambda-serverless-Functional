a6025d6d15b9e31323311075cceacd742224e25a
piercethevie <50924876+cb161769@users.noreply.github.com>
2021-06-13 00:26:13 -0400

    INITIAL automation cron-job

--- job/automation-cron-handler.js
+++ job/automation-cron-handler.js
@@ -4,6 +4,11 @@ const {getYesterdayDate,getTodaysDate,getTodaysDateWithMoreMinutes,changeDates} 
 const deviceName = config.deviceName;
 const {dynamoDBConnection} = require('../connections/connections');
 const {AutomateConsumption} =require('../helpers/automation/atuomationHelper');
+/**
+ * @function fetchTodaysData
+ * @author Claudio Raul Brito Mercedes
+ * @returns data
+ */
 async function fetchTodaysData(){
     try {
         const dates = changeDates(15,15);
@@ -24,8 +29,321 @@ async function fetchTodaysData(){
         }).promise();
         logger.log('info', `Requesting [fetchTodaysData]`, {tags: 'Method', additionalInfo: {operation: 'fetchYesterdaysData',databaseOperation:'GET', table: config.dynamoBB.deviceReadings.name }});
 
-        return data;
+        return data.Items;
     } catch (error) {
         logger.log('error', `Requesting [fetchTodaysData]`, {tags: 'Method', additionalInfo: {operation: 'fetchYesterdaysData',databaseOperation:'GET',error:e, table: config.dynamoBB.deviceReadings.name }});
+    }
+};
+async function fetchConfigurationData(){
+    const fareConfigurations =         [{
+        "deviceId": "ArduinoDevice01",
+        "registeredAt": "4/5/2021",
+        "updatedAt": "4/5/2021",
+        "status": true,
+    
+     
+        "configurationName": "Configuracion Uno",
+        "day": {
+            "type": "Buffer",
+            "data": []
+        },
+        MaximumKilowattsPerMonth: "5000000",
+        "connectionsConfigurations": [
+            {
+             connectionName:'Conexion 1',
+             "isItDaily":true,
+            "configurationMaximumKilowattsPerDay": "500000",
+            configurationMaximumKilowattsPerWeek: "5000000",
+            configurationMaximumKilowattsPerMonth: "5000000",
+            isItWeekly:false,
+            "configurationDays": [
+                {
+                    day: '',
+                    
+    
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                }
+            ],
+            isItMonthly:false,
+            configurationMonths:[
+                {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: ''   
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                }
+                ]   
+            },
+            {
+                connectionName:'Conexion 1',
+                "isItDaily":true,
+               "configurationMaximumKilowattsPerDay": "500000",
+               isItWeekly:false,
+               "configurationDays": [
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   }
+               ],
+               isItMonthly:false,
+               configurationMonths:[
+                   {
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: ''   
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   }
+                   ]   
+               },
+               {
+                connectionName:'Conexion 1',
+                "isItDaily":true,
+               "configurationMaximumKilowattsPerDay": "500000",
+               isItWeekly:false,
+               "configurationDays": [
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   }
+               ],
+               isItMonthly:false,
+               configurationMonths:[
+                   {
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: '',
+                   day: ''   
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   },
+                   {
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '',
+                       day: '' 
+                   }
+                   ]   
+               },
+               {
+             connectionName:'Conexion 1',
+             "isItDaily":true,
+            "configurationMaximumKilowattsPerDay": "500000",
+            isItWeekly:false,
+            "configurationDays": [
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                }
+            ],
+            isItMonthly:false,
+            configurationMonths:[
+                {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: ''   
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                },
+                {
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '',
+                    day: '' 
+                }
+                ]   
+            }
+        ],
+        "configurationId": "8288668a-9cb5-4ed7-bec2-dd9d7566cc93",
+        "isItDaily":true,
+        "configurationMaximumKilowattsPerDay": "500000",
+        isItWeekly:false,
+        "configurationDays": [
+            {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '' 
+            }
+        ],
+        isItMonthly:false,
+        configurationMonths:[
+            {
+             day: '',
+             day: '',
+             day: '',
+             day: '',
+             day: '',
+             day: '',
+             day: ''   
+            },
+            {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '' 
+            },
+            {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '' 
+            },
+            {
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '',
+                day: '' 
+            }
+        ]
+    
+    
+    }];
+    return fareConfigurations;
+};
+module.exports.handler = async (event, context,callback) =>{
+    try {
+        const data = await fetchTodaysData();
+        const configuration = await fetchConfigurationData();
+        await AutomateConsumption(data,configuration);
+        logger.log('info', `Requesting [automation-cron-job]`, {tags: 'automation-cron-job', additionalInfo: {operation: 'cron-job-handler', table: config.dynamoBB.deviceReadings.name }});
+    } catch (error) {
+        logger.log('error', `Requesting [automation-cron-job]`, {tags: 'automation-cron-job', additionalInfo: {operation: 'cron-job-handler',error:error, table: config.dynamoBB.deviceReadings.name }});   
     }
 }
